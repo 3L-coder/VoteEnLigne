114ed92f89264668e097b4e8072ffe6c42e78147
Abdoul <aktraore2008@gmail.com>
2019-05-01 22:38:32 +0800

    Update db.js

--- config/db.js
+++ config/db.js
@@ -4,6 +4,6 @@ const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 
 //connexion a mongoose mongodb
-mongoose.connect('mongodb://votingapp_user:votingapp_user@cluster0-shard-00-00-7hssh.mongodb.net:27017,cluster0-shard-00-01-7hssh.mongodb.net:27017,cluster0-shard-00-02-7hssh.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true')
+mongoose.connect('mongodb://dbUsename:dbPassword@cluster0-shard-00-00-7hssh.mongodb.net:27017,cluster0-shard-00-01-7hssh.mongodb.net:27017,cluster0-shard-00-02-7hssh.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true')
 .then(() => console.log('MongoDB connected'))
 .catch(err => console.log(err));
